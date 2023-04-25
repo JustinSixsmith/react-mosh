04c00d00460e8f4bd55268fe78d3b27c7f3ac47f
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-24 22:21:27 -0500

    Rebuilds components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import { useState } from 'react';
+import React from 'react';
 import Alert from './components/Alert';
-import Button from './components/Button';
 
 const App = () => {
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <Alert>My Alert!</Alert>
     </div>
   );
 };
 
 export default App;
+
+// import ListGroup from './components/ListGroup/ListGroup';
+
+// const App = () => {
+//   let items = ['New York', 'San Fransisco', 'Tokyo', 'London', 'Paris'];
+
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />    2
+//     </div>
+//   );
+// };
+
+// export default App;
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 interface Props {
-  items: string[];
+  items: [];
   heading: string;
   onSelectItem: (item: string) => void;
 }
@@ -12,7 +12,7 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
   return (
     <>
       <h1>{heading}</h1>
-      {items.length === 0 && <p>No item found</p>}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
