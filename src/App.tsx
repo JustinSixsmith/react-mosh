50e3d18da643e74bc6e68ba1adbcc2cbf0c6c75a
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-28 15:29:51 -0500

    Expands and collapses text with show more or less button

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-import { useState } from 'react';
+import ExpandableText from './components/ExpandableText';
 
 const App = () => {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      {
-        id: 1,
-        title: 'Product1',
-        quantity: 1,
-      },
-      {
-        id: 2,
-        title: 'Product2',
-        quantity: 1,
-      },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
   return (
     <div>
-      <br />
-      <button onClick={handleClick}>Click Me</button>
+      <ExpandableText maxChars={100}>Hello World</ExpandableText>
     </div>
   );
 };
 
 export default App;
+
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [cart, setCart] = useState({
+//     discount: 0.1,
+//     items: [
+//       {
+//         id: 1,
+//         title: 'Product1',
+//         quantity: 1,
+//       },
+//       {
+//         id: 2,
+//         title: 'Product2',
+//         quantity: 1,
+//       },
+//     ],
+//   });
+
+//   const handleClick = () => {
+//     setCart({
+//       ...cart,
+//       items: cart.items.map((item) =>
+//         item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+//       ),
+//     });
+//   };
+
+//   return (
+//     <div>
+//       <br />
+//       <button onClick={handleClick}>Click Me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
 
 // import { useState } from 'react';
 
