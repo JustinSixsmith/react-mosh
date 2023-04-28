fe3697265d0d63aac83c56f49448e09b938ad3e8
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-28 11:33:51 -0500

    Uses immer to change properties on bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,137 @@
-import Like from './components/Like';
+import { produce } from 'immer';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: 'Bug 1', fixed: false },
+    { id: 2, title: 'Bug 2', fixed: false },
+  ]);
+
+  const handleClick = () => {
+    // setBugs(bugs.map(bug => bug.id === 1 ? (...bug, { fixed: true }) : bug));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
+
   return (
     <div>
-      <Like onClick={() => console.log('Clicked')} />
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? 'Fixed' : 'New'}
+        </p>
+      ))}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
-}
+};
 
 export default App;
+
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [tags, setTags] = useState(['happy', 'good', 'bad']);
+
+//   const handleClick = () => {
+//     // Add
+//     setTags([...tags, 'exciting']);
+
+//     // Remove
+//     setTags(tags.filter((tag) => tag !== 'happy'));
+
+//     // Update
+//     setTags(tags.map((tag) => (tag === 'happy' ? 'goodness' : tag)));
+//   };
+
+//   return (
+//     <div>
+//       {tags.map((tag) => console.table(tag))}
+//       <button onClick={handleClick}>Click Me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [customer, setCustomer] = useState({
+//     name: 'John',
+//     address: {
+//       street: '123 Main St',
+//       city: 'New York',
+//       state: 'NY',
+//       zip: '10001',
+//     },
+//   });
+
+//   const handleClick = () => {
+//     setCustomer({
+//       ...customer,
+//       address: { ...customer.address, zip: '10002' },
+//     });
+//   };
+
+//   return (
+//     <div>
+//       {customer.address.zip}
+//       <button onClick={handleClick}>Click Me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [drink, setDrink] = useState({
+//     title: 'Americano',
+//     price: 5,
+//   });
+
+//   const handleClick = () => {
+//     setDrink({ ...drink, price: 10 });
+//   };
+
+//   return (
+//     <div>
+//       {drink.price}
+//       <button onClick={handleClick}>Click Me</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import Message from './Message';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Message />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import Like from './components/Like';
+
+// function App() {
+//   return (
+//     <div>
+//       <Like onClick={() => console.log('Clicked')} />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // import { BsFillCalendarFill } from 'react-icons/bs';
 
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,16 @@
-function Message() {
-  const name = 'Justin';
-  if (name) return <h1>Hello {name}</h1>;
-  return <h1>Hello World</h1>;
-}
+let count = 0;
+const Message = () => {
+  console.log('Message called');
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
+
+// function Message() {
+//   const name = 'Justin';
+//   if (name) return <h1>Hello {name}</h1>;
+//   return <h1>Hello World</h1>;
+// }
+
+// export default Message;
