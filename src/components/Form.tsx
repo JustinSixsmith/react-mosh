11baa055a888d23e9b6def6c8bb1c8782cb1fc0b
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-28 20:17:42 -0500

    Grabs input field content using ref hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { FormEvent, useRef } from 'react';
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: '', age: 0 };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
+          ref={nameRef}
           id="name"
           type="text"
           className="form-control"
@@ -19,6 +31,7 @@ const Form = () => {
           Age
         </label>
         <input
+          ref={ageRef}
           id="age"
           type="number"
           className="form-control"
