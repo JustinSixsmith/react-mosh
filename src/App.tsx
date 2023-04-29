80f13760f6473df77a0f252dd11cac721da1a488
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-29 14:01:56 -0500

    Builds expense table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import Form from './components/Form';
+import React, { useState } from 'react';
+import ExpenseList from './expense-tracker/components/ExpenseList';
 
 const App = () => {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: 'aaa',
+      amount: 10,
+      category: 'Utilities',
+    },
+    {
+      id: 2,
+      description: 'bbb',
+      amount: 10,
+      category: 'Utilities',
+    },
+    {
+      id: 3,
+      description: 'ccc',
+      amount: 10,
+      category: 'Utilities',
+    },
+    {
+      id: 4,
+      description: 'ddd',
+      amount: 10,
+      category: 'Utilities',
+    },
+  ]);
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
     </div>
   );
 };
 
 export default App;
+
+// import Form from './components/Form';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Form></Form>
+//     </div>
+//   );
+// };
+
+// export default App;
 
 // import ExpandableText from './components/ExpandableText';
 
