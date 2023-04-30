cc6d153830381b1f1dac7bfa92cc3c7983e7de0e
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-30 10:35:20 -0500

    Adds an error hook for axios request catch

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,24 @@ interface User {
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     axios
       .get<User[]>('https://jsonplaceholder.typicode.com/users')
       .then((res) => setUsers(res.data))
-      .catch((err) => console.log(err));
+      .catch((err) => setError(err.message));
   }, []);
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
   );
 };
 
