31cdd0e70a834829e88f65f0dfa4a14f4f7da2cf
Justin Sixsmith <justinsixsmith@gmail.com>
2023-04-30 22:33:55 -0500

    Extracts the User Service

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from 'react';
-import apiClient, { CanceledError } from './services/api-client';
-
-interface User {
-  id: number;
-  name: string;
-}
+import { CanceledError } from './services/api-client';
+import userService, { User } from './services/user-service';
 
 const App = () => {
   const [users, setUsers] = useState<User[]>([]);
@@ -12,13 +8,9 @@ const App = () => {
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    const controller = new AbortController();
-
     setLoading(true);
-    apiClient
-      .get<User[]>('/users', {
-        signal: controller.signal,
-      })
+    const { request, cancel } = userService.getAllUsers();
+    request
       .then((res) => {
         setUsers(res.data);
         setLoading(false);
@@ -28,14 +20,14 @@ const App = () => {
         setError(err.message);
         setLoading(false);
       });
-    return () => controller.abort();
+    return () => cancel();
   }, []);
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
 
-    apiClient.delete('/users/' + user.id).catch((err) => {
+    userService.deleteUser(user.id).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
@@ -50,8 +42,8 @@ const App = () => {
     };
     setUsers([newUser, ...users]);
 
-    apiClient
-      .post('/users', newUser)
+    userService
+      .createUser(newUser)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
@@ -64,7 +56,7 @@ const App = () => {
     const updatedUser = { ...user, name: user.name + '!' };
     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
 
-    apiClient.patch('/users/' + user.id, updatedUser).catch((err) => {
+    userService.updateUser(updatedUser).catch((err) => {
       setError(err.message);
       setUsers(originalUsers);
     });
