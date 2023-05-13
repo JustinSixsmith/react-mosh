86f245da553270b0bad2d06f7e7f72f831807c36
Justin Sixsmith <justinsixsmith@gmail.com>
2023-05-13 13:20:38 -0500

    Mutates data

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 10,
-    },
-  },
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
