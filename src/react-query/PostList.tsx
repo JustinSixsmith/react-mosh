4aeb76a6c865d63719a041b801c2067cb7219746
Justin Sixsmith <justinsixsmith@gmail.com>
2023-05-13 12:51:10 -0500

    Implements infinite queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  // const [userId, setUserId] = useState<number>();
   const pageSize = 10;
-  const [page, setPage] = useState(1);
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -13,35 +12,23 @@ const PostList = () => {
 
   return (
     <>
-      {/* <select
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        value={userId}
-        className="form-select mb-3"
-      >
-        <option value=""></option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select> */}
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
       <button
-        disabled={page === 1}
-        className="btn btn-primary my-3"
-        onClick={() => setPage(page - 1)}
-      >
-        Previous
-      </button>
-      <button
         className="btn btn-primary my-3 ms-1"
-        onClick={() => setPage(page + 1)}
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load more"}
       </button>
     </>
   );
